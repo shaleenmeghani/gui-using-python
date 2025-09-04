04424fe7ec2f52655e7fdb519250e16858e8f5c4
Shaleen Meghani <101651589+shaleenmeghani@users.noreply.github.com>
2025-09-04 16:12:44 +0530

    made it better..

--- 17_simple_interest.py
+++ 17_simple_interest.py
@@ -18,17 +18,26 @@ E2.grid(row=1,column=1)
 L3=Label(w,text="Enter Time",font=("cambria 20 bold"))
 L3.grid(row=2,column=0)
 
-L4=Label(w,text="",font=("cambria 20 bold"))
-L4.grid(row=4,column=0,sticky="N")
-
 E3=Entry(w,font=("cambria 20"))
 E3.grid(row=2,column=1)
+
 def calculation():
+    global L4
+    B1.configure(state="disabled")
+    B2.configure(state="normal")
     p=E1.get()
     r=E2.get()
     t=E3.get()
-    si=(int(p)*int(r)*int(t))/100
-    L4.config(text=f"SI = {si}")
+    si=(float(p)*float(r)*float(t))/100
+    L4=Label(w,text=f"SI = {si}")
+    L4.grid(row=4,column=0,columnspan=2)
+    
+def clear():
+    E1.delete(0, "end")
+    E2.delete(0, "end")
+    E3.delete(0, "end")
+    L4.grid_forget()
+    B1.configure(state="normal")
 
 def closewin():
     w.destroy()
@@ -36,7 +45,10 @@ def closewin():
 B1=Button(w,text="calculate",command=calculation)
 B1.grid(row=3,column=0)
 
-B2=Button(w,text="close window",command=closewin)
+B2=Button(w,text="clear",command=clear)
 B2.grid(row=3,column=1)
+
+B3=Button(w,text="close window",command=closewin)
+B3.grid(row=3,column=2)
 
 w.mainloop()
